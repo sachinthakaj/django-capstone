76a4844853bb6301a77966b3e18e98ba0c5474f4
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2026-03-10 11:56:25 +0530

    Remove unnecessary safe method check from IsProviderOwner permission class

--- apps/providers/permissions.py
+++ apps/providers/permissions.py
@@ -3,8 +3,5 @@ from rest_framework import permissions
 
 class IsProviderOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-
-        if request.method in permissions.SAFE_METHODS:
-            return True
         owner = getattr(obj, 'owner', None) or getattr(getattr(obj, 'provider', None), 'owner', None)
         return owner == request.user
